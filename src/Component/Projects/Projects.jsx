96e66b45d045deea1a074103dc835879b7e2fbf9
moktadir-mirul <moktadir.mirul@gmail.com>
2026-01-01 23:51:58 +0600

    projects number increased

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -279,6 +279,222 @@ const Projects = () => {
           </div>
         </div>
 
+        {/* Project 3 */}
+        <div className="border border-primary rounded-lg p-5 relative flex flex-col gap-5">
+          <div>
+            <img
+              src={P3}
+              alt="Project Image"
+              className="w-full h-full rounded-lg"
+            />
+          </div>
+          <div className="">
+            <div className="flex flex-col">
+              <h1 className="font-extrabold text-4xl epun tracking-wide">
+                Plant Companion
+              </h1>
+              <div className="flex flex-wrap items-center py-3 gap-3">
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg">
+                    <CgMediaLive /> Live
+                  </button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"> <FaGithub /> Github</button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"><CgDetailsMore /> Details</button>
+                </div>
+              </div>
+            </div>
+            <p className="font-medium text-lg text-justify py-1">
+              An Online Plant Care Tracker where users can add, edit, and manage plants with details like health status and watering schedules. Users can view plant details, and access to certain routes is restricted to authenticated users. . 
+            </p>
+            <p className="font-bold text-lg text-justify py-1">
+              → Secure Authentication & Role-Based Access <br />
+              → Pet Adoption & Donation Campaign Management <br />→ Fast,
+              Responsive UI with Infinite Scrolling & Dark Mode
+            </p>
+
+            <div className="flex flex-wrap gap-2 py-2">
+              <p className="px-3 py-1 bg-blue-200 text-blue-800 font-bold rounded-lg text-xl">
+                React
+              </p>
+              <p className="px-3 py-1 bg-gray-200 text-gray-800 font-bold rounded-lg text-xl">
+                Express JS
+              </p>
+              <p className="px-3 py-1 bg-green-200 text-green-800 font-bold rounded-lg text-xl">
+                Mongo DB
+              </p>
+              <p className="px-3 py-1 bg-fuchsia-200 text-fuchsia-800 font-bold rounded-lg text-xl">
+                JWT
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 3 */}
+        <div className="border border-primary rounded-lg p-5 relative flex flex-col gap-5">
+          <div>
+            <img
+              src={P3}
+              alt="Project Image"
+              className="w-full h-full rounded-lg"
+            />
+          </div>
+          <div className="">
+            <div className="flex flex-col">
+              <h1 className="font-extrabold text-4xl epun tracking-wide">
+                Plant Companion
+              </h1>
+              <div className="flex flex-wrap items-center py-3 gap-3">
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg">
+                    <CgMediaLive /> Live
+                  </button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"> <FaGithub /> Github</button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"><CgDetailsMore /> Details</button>
+                </div>
+              </div>
+            </div>
+            <p className="font-medium text-lg text-justify py-1">
+              An Online Plant Care Tracker where users can add, edit, and manage plants with details like health status and watering schedules. Users can view plant details, and access to certain routes is restricted to authenticated users. . 
+            </p>
+            <p className="font-bold text-lg text-justify py-1">
+              → Secure Authentication & Role-Based Access <br />
+              → Pet Adoption & Donation Campaign Management <br />→ Fast,
+              Responsive UI with Infinite Scrolling & Dark Mode
+            </p>
+
+            <div className="flex flex-wrap gap-2 py-2">
+              <p className="px-3 py-1 bg-blue-200 text-blue-800 font-bold rounded-lg text-xl">
+                React
+              </p>
+              <p className="px-3 py-1 bg-gray-200 text-gray-800 font-bold rounded-lg text-xl">
+                Express JS
+              </p>
+              <p className="px-3 py-1 bg-green-200 text-green-800 font-bold rounded-lg text-xl">
+                Mongo DB
+              </p>
+              <p className="px-3 py-1 bg-fuchsia-200 text-fuchsia-800 font-bold rounded-lg text-xl">
+                JWT
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 3 */}
+        <div className="border border-primary rounded-lg p-5 relative flex flex-col gap-5">
+          <div>
+            <img
+              src={P3}
+              alt="Project Image"
+              className="w-full h-full rounded-lg"
+            />
+          </div>
+          <div className="">
+            <div className="flex flex-col">
+              <h1 className="font-extrabold text-4xl epun tracking-wide">
+                Plant Companion
+              </h1>
+              <div className="flex flex-wrap items-center py-3 gap-3">
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg">
+                    <CgMediaLive /> Live
+                  </button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"> <FaGithub /> Github</button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"><CgDetailsMore /> Details</button>
+                </div>
+              </div>
+            </div>
+            <p className="font-medium text-lg text-justify py-1">
+              An Online Plant Care Tracker where users can add, edit, and manage plants with details like health status and watering schedules. Users can view plant details, and access to certain routes is restricted to authenticated users. . 
+            </p>
+            <p className="font-bold text-lg text-justify py-1">
+              → Secure Authentication & Role-Based Access <br />
+              → Pet Adoption & Donation Campaign Management <br />→ Fast,
+              Responsive UI with Infinite Scrolling & Dark Mode
+            </p>
+
+            <div className="flex flex-wrap gap-2 py-2">
+              <p className="px-3 py-1 bg-blue-200 text-blue-800 font-bold rounded-lg text-xl">
+                React
+              </p>
+              <p className="px-3 py-1 bg-gray-200 text-gray-800 font-bold rounded-lg text-xl">
+                Express JS
+              </p>
+              <p className="px-3 py-1 bg-green-200 text-green-800 font-bold rounded-lg text-xl">
+                Mongo DB
+              </p>
+              <p className="px-3 py-1 bg-fuchsia-200 text-fuchsia-800 font-bold rounded-lg text-xl">
+                JWT
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 3 */}
+        <div className="border border-primary rounded-lg p-5 relative flex flex-col gap-5">
+          <div>
+            <img
+              src={P3}
+              alt="Project Image"
+              className="w-full h-full rounded-lg"
+            />
+          </div>
+          <div className="">
+            <div className="flex flex-col">
+              <h1 className="font-extrabold text-4xl epun tracking-wide">
+                Plant Companion
+              </h1>
+              <div className="flex flex-wrap items-center py-3 gap-3">
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg">
+                    <CgMediaLive /> Live
+                  </button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"> <FaGithub /> Github</button>
+                </div>
+                <div>
+                  <button className="px-5 bg-gradient-to-r from-primary to-secondary cursor-pointer py-1  hover:bg-gradient-to-r hover:from-secondary hover:to-primary duration-200 transition-colors text-black font-bold text-xl shadow-md flex items-center gap-2 rounded-lg"><CgDetailsMore /> Details</button>
+                </div>
+              </div>
+            </div>
+            <p className="font-medium text-lg text-justify py-1">
+              An Online Plant Care Tracker where users can add, edit, and manage plants with details like health status and watering schedules. Users can view plant details, and access to certain routes is restricted to authenticated users. . 
+            </p>
+            <p className="font-bold text-lg text-justify py-1">
+              → Secure Authentication & Role-Based Access <br />
+              → Pet Adoption & Donation Campaign Management <br />→ Fast,
+              Responsive UI with Infinite Scrolling & Dark Mode
+            </p>
+
+            <div className="flex flex-wrap gap-2 py-2">
+              <p className="px-3 py-1 bg-blue-200 text-blue-800 font-bold rounded-lg text-xl">
+                React
+              </p>
+              <p className="px-3 py-1 bg-gray-200 text-gray-800 font-bold rounded-lg text-xl">
+                Express JS
+              </p>
+              <p className="px-3 py-1 bg-green-200 text-green-800 font-bold rounded-lg text-xl">
+                Mongo DB
+              </p>
+              <p className="px-3 py-1 bg-fuchsia-200 text-fuchsia-800 font-bold rounded-lg text-xl">
+                JWT
+              </p>
+            </div>
+          </div>
+        </div>
+
         {/* Project ends */}
       </div>
     </div>
